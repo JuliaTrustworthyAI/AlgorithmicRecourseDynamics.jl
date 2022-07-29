5c2f2ecc0250d815fda89b1c71ff6918b6057e1d
pat-alt <altmeyerpat@gmail.com>
2022-07-29 09:37:54 +0200

    more work on gravitational generator. It works as expected if we choose strict convergence: we consider concergence as when the probability threshold is reached AND the reduction in our loss function from one iteration to another is small.

--- src/generators/functions.jl
+++ src/generators/functions.jl
@@ -1,11 +1,13 @@
 using LinearAlgebra, CounterfactualExplanations
 
-struct GravitationalGenerator <: AbstractGradientBasedGenerator
+mutable struct GravitationalGenerator <: AbstractGradientBasedGenerator
     loss::Union{Nothing,Symbol} # loss function
     complexity::Function # complexity function
     λ::Union{AbstractFloat,AbstractVector} # strength of penalty
     ϵ::AbstractFloat # learning rate
     τ::AbstractFloat # tolerance for convergence
+    K::Int # number of K randomly chosen neighbours
+    centroid::Union{Nothing,AbstractArray}
 end
 
 # API streamlining:
@@ -13,6 +15,8 @@ using Parameters
 @with_kw struct GravitationalGeneratorParams
     ϵ::AbstractFloat=0.1
     τ::AbstractFloat=1e-5
+    K::Int=5
+    centroid::Union{Nothing,AbstractArray}=nothing
 end
 
 """
@@ -34,19 +38,11 @@ generator = GravitationalGenerator()
 """
 function GravitationalGenerator(;loss::Union{Nothing,Symbol}=nothing,complexity::Function=norm,λ::Union{AbstractFloat,AbstractVector}=[0.1, 1.0],kwargs...)
     params = GravitationalGeneratorParams(;kwargs...)
-    GravitationalGenerator(loss, complexity, λ, params.ϵ, params.τ)
+    GravitationalGenerator(loss, complexity, λ, params.ϵ, params.τ, params.K, params.centroid)
 end
 
 # Complexity:
 using Statistics, LinearAlgebra
-function gravity(counterfactual_state::State; K=5)
-    ids = rand(1:size(counterfactual_state.params[:potential_neighbours],2),K)
-    neighbours = counterfactual_state.params[:potential_neighbours][:,ids]
-    centroid = mean(neighbours, dims=2)
-    gravity_ = norm(centroid .- counterfactual_state.f(counterfactual_state.s′))
-    return gravity_
-end
-
 using CounterfactualExplanations.CounterfactualState
 import CounterfactualExplanations.Generators: h
 """
@@ -55,8 +51,20 @@ import CounterfactualExplanations.Generators: h
 The default method to apply the generator complexity penalty to the current counterfactual state for any generator.
 """
 function h(generator::GravitationalGenerator, counterfactual_state::CounterfactualState.State)
+    
+    # Distance from factual:
     dist_ = generator.complexity(counterfactual_state.x .- counterfactual_state.f(counterfactual_state.s′))
-    gravity_ = gravity(counterfactual_state)
+
+    # Gravitational center:
+    if isnothing(generator.centroid)
+        ids = rand(1:size(counterfactual_state.params[:potential_neighbours],2),generator.K)
+        neighbours = counterfactual_state.params[:potential_neighbours][:,ids]
+        generator.centroid = mean(neighbours, dims=2)
+    end
+
+    # Distance from gravitational center:
+    gravity_ = generator.complexity(generator.centroid .- counterfactual_state.f(counterfactual_state.s′))
+    
     if length(generator.λ)==1
         penalty = generator.λ * (dist_ .+ gravity_)
     else

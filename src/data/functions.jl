4e73a669261218609a59cac2f2bd93eee92c877a
pat-alt <altmeyerpat@gmail.com>
2022-12-22 16:24:03 +0100

    moved stuff out of here

--- src/data/functions.jl
+++ src/data/functions.jl
@@ -1,47 +1,4 @@
-using LazyArtifacts
-using CounterfactualExplanations.DataPreprocessing: CounterfactualData
-using CSV
-using DataFrames
-using Serialization
 using StatsBase
-
-function load_synthetic(max_obs::Union{Nothing,Int}=nothing)
-    data_dir = joinpath(artifact"data", "data/synthetic")
-    files = readdir(data_dir)
-    files = files[contains.(files, ".csv")]
-    data = map(files) do file
-        df = CSV.read(joinpath(data_dir, file), DataFrame)
-        X = convert(Matrix, hcat(df.x1, df.x2)')
-        y = convert(Matrix, df.target')
-        data = CounterfactualData(X, y)
-        if !isnothing(max_obs)
-            n_classes = length(unique(y))
-            data = undersample(data, Int(round(max_obs / n_classes)))
-        end
-        (Symbol(replace(file, ".csv" => "")) => data)
-    end
-    data = Dict(data...)
-    return data
-end
-
-function load_real_world(max_obs::Union{Nothing,Int}=nothing; data_dir::Union{Nothing, String}=nothing)
-    if isnothing(data_dir)
-        data_dir = joinpath(artifact"data", "data/real_world")
-    end
-    files = readdir(data_dir)
-    files = files[contains.(files, ".jls")]
-    data = map(files) do file
-        counterfactual_data = Serialization.deserialize(joinpath(data_dir, file))
-        if !isnothing(max_obs)
-            n_classes = length(unique(counterfactual_data.y))
-            counterfactual_data = undersample(counterfactual_data, Int(round(max_obs / n_classes)))
-        end
-        (Symbol(replace(file, ".jls" => "")) => counterfactual_data)
-    end
-    data = Dict(data...)
-    return data
-end
-
 
 function scale(X, dim)
     dt = fit(ZScoreTransform, X, dim=dim)

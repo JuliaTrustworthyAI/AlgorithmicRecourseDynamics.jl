38a04c481f8bbeb6c7a320d9d2d94b48c6878f4f
pat-alt <altmeyerpat@gmail.com>
2022-12-23 16:35:45 +0100

    uh

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,41 @@ import CompatHelperLocal as CHL
 CHL.@check()
 
 using AlgorithmicRecourseDynamics
+using AlgorithmicRecourseDynamics.Data
+using AlgorithmicRecourseDynamics.Experiments
+using AlgorithmicRecourseDynamics.Models
+using AlgorithmicRecourseDynamics: run!
+using CounterfactualExplanations
+using Flux
+using MLJBase
+using Plots
+using Random
 using Test
 
 @testset "AlgorithmicRecourseDynamics.jl" begin
-    # Write your tests here.
+
+    N = 1000
+    xmax = 2
+    X, ys = make_blobs(
+        N, 2;
+        centers=2, as_table=false, center_box=(-xmax => xmax), cluster_std=0.1
+    )
+    ys .= ys .== 2
+    X = X'
+    counterfactual_data = CounterfactualData(X, ys')
+
+    n_epochs = 100
+    model = Chain(Dense(2, 1))
+    mod = FluxModel(model)
+    generator = GenericGenerator()
+
+    data_train, data_test = Data.train_test_split(counterfactual_data)
+    Models.train(mod, data_train; n_epochs=n_epochs)
+
+    models = Dict(:mymodel => mod)
+    generators = Dict(:wachter => generator)
+    experiment = set_up_experiment(data_train, data_test, models, generators)
+
+    run!(experiment)
+
 end

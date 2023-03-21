e55e0e35691bf34147d00c0510675ba9ac08fab7
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2023-03-21 18:48:11 +0100

    Fix a typo

--- src/data/utils.jl
+++ src/data/utils.jl
@@ -9,7 +9,7 @@ function Base.hcat(data::CounterfactualData, more_data::CounterfactualData)
     more_data = deepcopy(more_data)
 
     @assert all(data.features_categorical .== more_data.features_categorical) "Datasets have different categorical indices."
-    @assert all(data.features_continuous .== more_data.features_continuous) "Datasets have different continous indices."
+    @assert all(data.features_continuous .== more_data.features_continuous) "Datasets have different continuous indices."
 
     data.X = hcat(data.X, more_data.X)
     data.y = hcat(data.y, more_data.y)
